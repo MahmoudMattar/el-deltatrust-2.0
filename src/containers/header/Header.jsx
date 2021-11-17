76cc30c2090007dbed09c94e00be61b6071efd41
mahmoud mattar <mahmoud96mattar@gmail.com>
2021-11-17 13:18:48 +0200

    update container

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
         Let&apos;s Build Something amazing with GPT-3 OpenAI
       </h1>
       <p>
-        Yet bed any for travelling assistance indulgence unpleasing. Not
+        Yet bed any for travelling assistancfe indulgence unpleasing. Not
         thoughts all exercise blessing. Indulgence way everything joy alteration
         boisterous the attachment. Party we years to order allow asked of.
       </p>
